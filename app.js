5dbd2c67bd9e922e11e299d2aee721b4949a75b0
Leandro <syiabonga111@gmail.com>
2019-10-17 14:58:24 -0500

    Method to change questions

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 new Vue({
   el: '#app',
   data:{
-    gameIsRunning: false
+    gameIsRunning: false,
+    number: 1,
+    questions: [
+      { number: 1,content: " Question 1" },
+      { number: 2,content: " Question 2" },
+      { number: 3,content: " Question 3" },
+      { number: 4,content: " Question 4" }
+    ]
   },
   methods: {
     startGame: function(){
       this.gameIsRunning = true;
+    },
+    changeQuestion: function(number){
+      if (this.number == 4) {
+        return;
+      }
+      return this.number += 1;
     }
   }
 });
