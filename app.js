39af76f8e8c5db272d65a8933235e5510bbe909b
Leandro <syiabonga111@gmail.com>
2019-10-18 00:15:33 -0500

    Check if an answer is correct and gives its result

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ new Vue({
   data:{
     gameIsRunning: false,
     number: 1,
+    picked: "",
     questions: [
-      { number: 1,content: " Question 1" },
+      { number: 1,content: " for(i=0,i>=10;i++)",output1:"Output: {1,2,3,4,5,6,7,8,9,10}", output2:"Output: {10,9,8,7,6,5,4,3,2,1}", answer: "one"},
       { number: 2,content: " Question 2" },
       { number: 3,content: " Question 3" },
       { number: 4,content: " Question 4" }
@@ -14,10 +15,11 @@ new Vue({
     startGame: function(){
       this.gameIsRunning = true;
     },
-    changeQuestion: function(number){
+    changeQuestion: function(){
       if (this.number == 4) {
         return;
       }
+      alert('Correct answer');
       return this.number += 1;
     }
   }
