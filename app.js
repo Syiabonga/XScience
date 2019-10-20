db403307ff616c086439dafcca845fa3d14d98b3
Leandro <syiabonga111@gmail.com>
2019-10-19 23:12:17 -0500

    Interact with the user about his progress in the game

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ new Vue({
     gameIsRunning: false,
     number: 1,
     picked: "",
+    points:0,
+    attempts: 3,
     questions: [
       { number: 1,content: " for(i=0,i>=10;i++)",output1:"Output: {1,2,3,4,5,6,7,8,9,10}", output2:"Output: {10,9,8,7,6,5,4,3,2,1}", answer: "one"},
       { number: 2,content: " Question 2" },
@@ -15,12 +17,23 @@ new Vue({
     startGame: function(){
       this.gameIsRunning = true;
     },
-    changeQuestion: function(){
+    changeQuestion: function(event,answer){
       if (this.number == 4) {
-        return;
+        return alert('Congratulations');
       }
-      alert('Correct answer');
-      return this.number += 1;
+      if (this.picked == answer) {
+        alert('Correct answer!');
+        this.points += 1;
+        return this.number += 1;
+      }else {
+        this.attempts -=1;
+        alert(' Incorrect answer!');
+        if (this.attempts <= 0) {
+          alert('You lost!');
+          this.points = 0;
+          this.number = 1;
+        }
+      }
     }
   }
 });
